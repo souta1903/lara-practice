086382e6abb4cd1b195c17c22703a1216bc79aab
souta1903 <93124330+souta1903@users.noreply.github.com>
2022-01-02 19:54:44 +0900

    ルートパラメータなどの学習

---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+    <title>Hello/Index</title>
+</head>
+<style>
+    body{font-size: 16pt; color: #999;}
+    h1{ font-size: 100pt; text-align: right; color: #eee;
+    margin: -40px 0px -50px 0px}
+    </style>
+<body>
+    <h1>Hello</h1>
+    <h3>Request</h3>
+    <pre>{$request}</pre>
+    <h3>Response</h3>
+    <pre>{$response}</pre>
+</body>
+</html>
